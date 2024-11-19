c6d693479929afdb3cbc3022551136467190bd6b
1O$K <74111513+1OSK@users.noreply.github.com>
2024-11-19 22:17:41 +0300

    еще чучуть

--- datacenter/src/pages/EquipmentListDatacenter.tsx
+++ datacenter/src/pages/EquipmentListDatacenter.tsx
@@ -19,8 +19,7 @@ export const EquipmentListDatacenter: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-
-  const defaultImageUrl = 'https://1osk.github.io/Frontend/images/2.png';
+  const defaultImageUrl = 'https://1osk.github.io/Frontend/images/default.png';
 
   const breadcrumbItems = [
     { label: 'Главная', path: '/' },
@@ -28,12 +27,6 @@ export const EquipmentListDatacenter: React.FC = () => {
   ];
 
   useEffect(() => {
-    const storedMinPrice = localStorage.getItem('minPrice');
-    const storedMaxPrice = localStorage.getItem('maxPrice');
-    
-    if (storedMinPrice) dispatch(setMinPrice(storedMinPrice));
-    if (storedMaxPrice) dispatch(setMaxPrice(storedMaxPrice));
-
     fetchServices();
   }, []);
 
@@ -63,10 +56,6 @@ export const EquipmentListDatacenter: React.FC = () => {
     fetchServices();
   };
 
-
-
-
-
   return (
     <>
       <Navbar />
@@ -76,28 +65,27 @@ export const EquipmentListDatacenter: React.FC = () => {
 
         <div className="breadcrumb-controls">
           <Form className="search-form d-flex" onSubmit={handleSearch}>
-          <Form.Control
-            type="number"
-            placeholder="Минимальная цена..."
-            value={minPrice}
-            onChange={(e) => dispatch(setMinPrice(e.target.value))} 
-            min="0"
-            className="search-input"
-          />
+            <Form.Control
+              type="number"
+              placeholder="Минимальная цена..."
+              value={minPrice}
+              onChange={(e) => dispatch(setMinPrice(e.target.value))} 
+              min="0"
+              className="search-input"
+            />
 
-          <Form.Control
-            type="number"
-            placeholder="Максимальная цена..."
-            value={maxPrice}
-            onChange={(e) => dispatch(setMaxPrice(e.target.value))}
-            min="0"
-            className="search-input"
-          />
+            <Form.Control
+              type="number"
+              placeholder="Максимальная цена..."
+              value={maxPrice}
+              onChange={(e) => dispatch(setMaxPrice(e.target.value))}
+              min="0"
+              className="search-input"
+            />
             <Button variant="primary" type="submit" className="search-button" disabled={loading}>
               {loading ? <Spinner animation="border" size="sm" /> : 'Поиск'}
             </Button>
           </Form>
-          {/* Кнопка корзины */}
           <div className="order-info">
             <span className="current-order-button disabled">Текущий заказ недоступен</span>
           </div>
@@ -105,32 +93,32 @@ export const EquipmentListDatacenter: React.FC = () => {
       </div>
 
       <Container className="space">
-  {loading && <Spinner animation="border" />}
-  {error && <Alert variant="danger">{error}</Alert>}
-  <Row>
-    {services.map(service => (
-      <Col key={service.id} xs={12} sm={6} md={5} lg={4} className="mb-4">
-        <div className="card">
-          <Link to={`/datacenter-services/${service.id}/`} className="card-title">
-            <p className="title">{service.name}</p>
-          </Link>
-          <div className="image-container">
-            <img src={service.image_url || defaultImageUrl} alt={service.name} className="service-image" />
-          </div>
-          <div className="card-price-button-container" style={{ marginTop: 'auto' }}>
-            {service.price && <p className="price">{service.price} руб.</p>}
-            <div className="button-container">
-              <Link to={`/datacenter-services/${service.id}/`} className="card-button">Подробнее о комплектующем</Link>
-              <div className="add-button-container">
-                <Button variant="secondary" disabled className="card-button disabled">Добавить в заказ</Button>
+        {loading && <Spinner animation="border" />}
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Row>
+          {services.map(service => (
+            <Col key={service.id} xs={12} sm={6} md={5} lg={4} className="mb-4">
+              <div className="card">
+                <Link to={`/datacenter-services/${service.id}/`} className="card-title">
+                  <p className="title">{service.name}</p>
+                </Link>
+                <div className="image-container">
+                  <img src={service.image_url || defaultImageUrl} alt={service.name} className="service-image" />
+                </div>
+                <div className="card-price-button-container" style={{ marginTop: 'auto' }}>
+                  {service.price && <p className="price">{service.price} руб.</p>}
+                  <div className="button-container">
+                    <Link to={`/datacenter-services/${service.id}/`} className="card-button">Подробнее о комплектующем</Link>
+                    <div className="add-button-container">
+                      <Button variant="secondary" disabled className="card-button disabled">Добавить в заказ</Button>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </Col>
-    ))}
-  </Row>
-</Container>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
   );
 };

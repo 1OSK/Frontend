a92cff83669414d3a5206dafcd1dc96f41713125
1O$K <74111513+1OSK@users.noreply.github.com>
2024-11-25 23:55:01 +0300

    вроде финал

--- datacenter/src/App.tsx
+++ datacenter/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
--- datacenter/vite.config.ts
+++ datacenter/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  base: "/Frontend",
   logLevel: 'info',
   build: {
     outDir: 'dist', // Убедитесь, что путь для вывода правильный
